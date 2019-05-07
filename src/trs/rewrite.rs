64cd2c8c60259d5c045f60f868f97bf488cd5bcb
na2021 <na2021@mit.edu>
2019-05-07 15:07:15 -0400

    Rewrite: Moving tests for private methods from doctests to normal tests

--- src/trs/rewrite.rs
+++ src/trs/rewrite.rs
@@ -305,32 +305,10 @@ impl TRS {
         }
     }
     /// Replaces one subterm with another subterm in a main Term.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// # #[macro_use] extern crate polytype;
-    /// # extern crate programinduction;
-    /// # extern crate rand;
-    /// # extern crate term_rewriting;
-    /// # use programinduction::trs::{TRS, Lexicon};
-    /// # use rand::{thread_rng};
-    /// # use term_rewriting::{Context, RuleContext, Signature, parse_rule, parse_term};
-    /// # fn main() {
-    /// let mut sig = Signature::default();
-    /// let term = parse_term(&mut sig, "A(y_ B(C) D)").expect("parse of A(y_ B(C) D)");
-    /// let t = parse_term(&mut sig, "B(C)").expect("parse of B(C)");
-    /// let v = parse_term(&mut sig, "x_").expect("parse of x_");
-    ///
-    /// let new_term = TRS::replace_term_helper(&term, &t, v);
-    ///
-    /// assert_eq!(new_term.display(), "A(y_ x_ D)");
-    /// # }
-    /// ```
-    pub fn replace_term_helper(term: &Term, t: &Term, v: Term) -> Term {
-        if Term::alpha(t, term) != None {
-            let unwrapped_sub = Term::alpha(t, term).unwrap();
-            return v.substitute(&unwrapped_sub);
+    fn replace_term_helper(term: &Term, replace: &Term, new: Term) -> Term {
+        if Term::alpha(replace, term) != None {
+            let unwrapped_sub = Term::alpha(replace, term).unwrap();
+            return new.substitute(&unwrapped_sub);
         } else if term.args() != vec![] {
             match *term {
                 Term::Variable(ref _var) => {
@@ -339,7 +317,7 @@ impl TRS {
                 Term::Application { ref op, args: _ } => {
                     let mut args = term.args().clone();
                     for idx in 0..args.len() {
-                        args[idx] = TRS::replace_term_helper(&args[idx], t, v.clone());
+                        args[idx] = TRS::replace_term_helper(&args[idx], replace, new.clone());
                     }
                     let op = op.clone();
                     return Term::Application { op, args };
@@ -349,33 +327,7 @@ impl TRS {
         return term.clone();
     }
     /// Replaces one subterm with another subterm in a given Rule.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// # #[macro_use] extern crate polytype;
-    /// # extern crate programinduction;
-    /// # extern crate rand;
-    /// # extern crate term_rewriting;
-    /// # use programinduction::trs::{TRS, Lexicon};
-    /// # use rand::{thread_rng};
-    /// # use term_rewriting::{Context, RuleContext, Signature, parse_rule, parse_term};
-    /// # fn main() {
-    /// let mut sig = Signature::default();
-    /// let rule = parse_rule(&mut sig, "A(y_ B(C) D) = B(C)").expect("parse of A(y_ B(C) D) = B(C)");
-    /// let t = parse_term(&mut sig, "B(C)").expect("parse of B(C)");
-    /// let v = parse_term(&mut sig, "x_").expect("parse of x_");
-    ///
-    /// let new_rule = TRS::replace_term_in_rule_helper(&rule, &t, v);
-    /// if new_rule == None {
-    ///     assert!(false);
-    /// } else {
-    ///     let rule = new_rule.unwrap();
-    ///     assert_eq!(rule.display(), "A(y_ x_ D) = x_");
-    /// }
-    /// # }
-    /// ```
-    pub fn replace_term_in_rule_helper(rule: &Rule, t: &Term, v: Term) -> Option<Rule> {
+    fn replace_term_in_rule_helper(rule: &Rule, t: &Term, v: Term) -> Option<Rule> {
         let r = rule.clone();
         let lhs = TRS::replace_term_helper(&r.lhs, t, v.clone());
         let mut rhs: Vec<Term> = vec![];
@@ -386,68 +338,43 @@ impl TRS {
     }
     // helper for routinization
     // TODO fix rule lhs before inserting
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// # #[macro_use] extern crate polytype;
-    /// # extern crate programinduction;
-    /// # extern crate rand;
-    /// # extern crate term_rewriting;
-    /// # use programinduction::trs::{TRS, Lexicon};
-    /// # use rand::{thread_rng};
-    /// # use term_rewriting::{Context, RuleContext, Signature, parse_rule, parse_term};
-    /// # fn main() {
-    /// let mut sig = Signature::default();
-    ///
-    /// let t = parse_term(&mut sig, "A(B(x_ x_))").expect("parse of A(B(x_ x_))");
-    /// let r = parse_rule(&mut sig, "C(y_) = B(y_ y_)").expect("parse of C(y_) = B(y_ y_)");
-    /// let mut rng = thread_rng();
-    ///
-    /// let result = TRS::inverse_evaluation_helper(&r, &t, &mut rng);
-    ///
-    /// assert_eq!(result.display(), "A(C(x_))");
-    /// # }
-    /// ```
-    pub fn inverse_evaluation_helper<R: Rng>(rule: &Rule, t: &Term, rng: &mut R) -> Term {
+    fn inverse_evaluation_helper<R: Rng>(rule: &Rule, t: &Term, rng: &mut R) -> Term {
         let rhs_idx = rng.gen_range(0, rule.rhs.len());
         TRS::replace_term_helper(t, &rule.rhs[rhs_idx], rule.lhs.clone())
     }
     /// Given two rules, attempts to apply one rule inversely to the other.
-    ///
-    /// # Example
-    ///
-    /// ```
-    /// # #[macro_use] extern crate polytype;
-    /// # extern crate programinduction;
-    /// # extern crate rand;
-    /// # extern crate term_rewriting;
-    /// # use programinduction::trs::{TRS, Lexicon};
-    /// # use rand::{thread_rng};
-    /// # use term_rewriting::{Context, RuleContext, Signature, parse_rule, parse_term};
-    /// # fn main() {
-    /// let mut sig = Signature::default();
-    ///
-    /// let r = parse_rule(&mut sig, "A(B(x_ x_)) = D B(x_ x_)").expect("parse of A(B(x_ x_)) = D B(x_ x_)");
-    /// let rule = parse_rule(&mut sig, "C(y_) = B(y_ y_)").expect("parse of C(y_) = B(y_ y_)");
-    /// let mut rng = thread_rng();
-    ///
-    /// let result = TRS::inverse_evaluate_rule_helper(&rule, &r, &mut rng);
-    ///
-    /// assert_eq!(result.unwrap().pretty(), "A(C(x_)) = D C(x_)");
-    /// # }
-    /// ```
-    pub fn inverse_evaluate_rule_helper<R: Rng>(
-        rule: &Rule,
-        r: &Rule,
-        rng: &mut R,
-    ) -> Option<Rule> {
+    fn inverse_evaluate_rule_helper<R: Rng>(rule: &Rule, r: &Rule, rng: &mut R) -> Option<Rule> {
         let lhs = TRS::inverse_evaluation_helper(rule, &r.lhs, rng);
         let mut rhs: Vec<Term> = vec![];
         for idx in 0..r.rhs.len() {
             rhs.push(TRS::inverse_evaluation_helper(rule, &r.rhs[idx], rng));
         }
         Rule::new(lhs, rhs)
+    }
+
+    /// Extracts a sngle rule from the TRS selecting only 1 rhs
+    fn sample_rule<R: Rng>(&self, rng: &mut R) -> Result<Rule, SampleError> {
+        let mut trs = self.clone();
+        let num_rules = self.len();
+        let num_background = self
+            .lex
+            .0
+            .read()
+            .expect("poisoned lexicon")
+            .background
+            .len();
+        let ref_idx: usize = rng.gen_range(0, num_rules);
+        let mut ref_rule = trs.utrs.rules[ref_idx].clone();
+        let rhs_idx = rng.gen_range(0, ref_rule.rhs.len());
+        let temp_rule = Rule::new(ref_rule.lhs, vec![ref_rule.rhs[rhs_idx].clone()]);
+        if temp_rule == None {
+            return Err(SampleError::OptionsExhausted);
+        }
+        ref_rule = temp_rule.unwrap();
+        if ref_rule.lhs.variables().len() < ref_rule.rhs[0].variables().len() {
+            return Err(SampleError::OptionsExhausted);
+        }
+        Ok(ref_rule)
     }
     /// Selects two Rules from the TRS at random and atempts to inverse evaluate one rule on the other, if it
     /// succeeds it takes that new rule and inserts it imediately after the background.
@@ -462,31 +389,20 @@ impl TRS {
             .background
             .len();
         if num_background >= num_rules - 1 {
-            return Ok(trs);
+            return Err(SampleError::OptionsExhausted);
         }
-        let ref_idx: usize = rng.gen_range(0, num_rules);
         let mut target_idx: usize = rng.gen_range(num_background, num_rules);
-        while ref_idx == target_idx {
-            target_idx = rng.gen_range(num_background, num_rules);
-        }
-        let mut ref_rule = trs.utrs.rules[ref_idx].clone();
-        let rhs_idx = rng.gen_range(0, ref_rule.rhs.len());
-        let temp_rule = Rule::new(ref_rule.lhs, vec![ref_rule.rhs[rhs_idx].clone()]);
-        if temp_rule == None {
-            return Ok(trs);
-        }
-        ref_rule = temp_rule.unwrap();
-        if ref_rule.lhs.variables().len() != ref_rule.rhs[0].variables().len() {
-            return Ok(trs);
-        }
+        let ref_rule = trs.sample_rule(rng)?;
         let new_rule =
             TRS::inverse_evaluate_rule_helper(&ref_rule, &trs.utrs.rules[target_idx], rng);
         if new_rule == None {
-            return Ok(trs);
+            return Err(SampleError::OptionsExhausted);
         }
-        trs.utrs.remove_idx(target_idx).expect("removing old rule");
         trs.utrs
-            .insert_idx(target_idx, new_rule.unwrap())
+            .remove_clauses(&ref_rule)
+            .expect("removing old rule");
+        trs.utrs
+            .insert_clauses(&new_rule.unwrap())
             .expect("inserting new rule");
         Ok(trs)
     }
@@ -511,4 +427,69 @@ impl fmt::Display for TRS {
 
         write!(f, "{}", trs_str)
     }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    extern crate polytype;
+    extern crate rand;
+    extern crate term_rewriting;
+    use rand::thread_rng;
+    use term_rewriting::{parse_rule, parse_term, Context, RuleContext, Signature};
+
+    #[test]
+    fn replace_term_helper_test() {
+        let mut sig = Signature::default();
+        let term = parse_term(&mut sig, "A(y_ B(C) D)").expect("parse of A(y_ B(C) D)");
+        let t = parse_term(&mut sig, "B(C)").expect("parse of B(C)");
+        let v = parse_term(&mut sig, "x_").expect("parse of x_");
+
+        let new_term = TRS::replace_term_helper(&term, &t, v);
+        assert_eq!(new_term.display(), "A(y_ x_ D)");
+    }
+
+    #[test]
+    fn replace_term_in_rule_helper_test() {
+        let mut sig = Signature::default();
+        let rule =
+            parse_rule(&mut sig, "A(y_ B(C) D) = B(C)").expect("parse of A(y_ B(C) D) = B(C)");
+        let t = parse_term(&mut sig, "B(C)").expect("parse of B(C)");
+        let v = parse_term(&mut sig, "x_").expect("parse of x_");
+
+        let new_rule = TRS::replace_term_in_rule_helper(&rule, &t, v);
+        if new_rule == None {
+            assert!(false);
+        } else {
+            let rule = new_rule.unwrap();
+            assert_eq!(rule.display(), "A(y_ x_ D) = x_");
+        }
+    }
+
+    #[test]
+    fn inverse_evaluation_helper_test() {
+        let mut sig = Signature::default();
+
+        let t = parse_term(&mut sig, "A(B(x_ x_))").expect("parse of A(B(x_ x_))");
+        let r = parse_rule(&mut sig, "C(y_) = B(y_ y_)").expect("parse of C(y_) = B(y_ y_)");
+        let mut rng = thread_rng();
+
+        let result = TRS::inverse_evaluation_helper(&r, &t, &mut rng);
+
+        assert_eq!(result.display(), "A(C(x_))");
+    }
+
+    #[test]
+    fn inverse_evluate_rule_helper_test() {
+        let mut sig = Signature::default();
+
+        let r = parse_rule(&mut sig, "A(B(x_ x_)) = D B(x_ x_)")
+            .expect("parse of A(B(x_ x_)) = D B(x_ x_)");
+        let rule = parse_rule(&mut sig, "C(y_) = B(y_ y_)").expect("parse of C(y_) = B(y_ y_)");
+        let mut rng = thread_rng();
+
+        let result = TRS::inverse_evaluate_rule_helper(&rule, &r, &mut rng);
+
+        assert_eq!(result.unwrap().pretty(), "A(C(x_)) = D C(x_)");
+    }
+
 }
